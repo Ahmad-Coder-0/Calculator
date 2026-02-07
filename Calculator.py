9e904706394b7bb219dc231203b837c710317460
Ahmad-Coder <ahmadcub9@gmail.com>
2026-02-07 18:11:25 +0330

    this a simple Calculator

--- Calculator.py
+++ Calculator.py
@@ -1,10 +1,23 @@
-import os, time
+import os
+import time
 
 OS = os.name
 cmd_cleaner = 'cls' if OS == 'nt' else 'clear'
 
+
 def Calculator(x, y, op):
-    pass
+    match op:
+        case '+':
+            return x + y
+        case '-':
+            return x - y
+        case '*':
+            return x * y
+        case '/':
+            return x / y
+        case _:
+            print("invalid operator!!")
+
 
 print("Welcome!!!")
 for i in range(1, 6):
@@ -22,4 +35,3 @@ while True:
             break
     second_int = int(input("Enter second integer: "))
     Calculator(first_int, second_int, op)
-    
